d38d8ad0334ed48f75262352a36f42178a27c4ba
k <k@DESKTOP-RK9L96K.bbrouter>
2024-07-16 10:02:20 +0530

    learn to commit code

---
+++ src/Methods.java
@@ -0,0 +1,15 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+//Methods a = new Methods();
+demo();
+
+
+	}
+	public static void demo()
+	{
+ System.out.println("vijay will call the methods");
+}
+}
